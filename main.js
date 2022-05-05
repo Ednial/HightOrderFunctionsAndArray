1e387028cc07dfa39df0cc2d6f6d5998f6298b47
Ednial <ericxonb@gmail.com>
2022-05-05 07:41:35 -0500

    FirstCommit: Made twoArrays

---
+++ main.js
@@ -0,0 +1,14 @@
+const companies=[{name:"Company one",category:"Finance",start:1981,end:2003},
+{name:"Company one",category:"Finance",start:1981,end:2003},
+{name:"Company two",category:"Retail",start:1998,end:2003},
+{name:"Company three",category:"Auto",start:2016,end:2019},
+{name:"Company four",category:"Retail",start:1971,end:2022},
+{name:"Company five",category:"Technology",start:1991,end:2007},
+{name:"Company six",category:"Finance",start:2000,end:2003},
+{name:"Company seven",category:"Auto",start:2015,end:2019},
+{name:"Company eight",category:"Technology",start:2002,end:2007},
+{name:"Company nine",category:"Retail",start:1985,end:2003}
+];
+
+const ages=[12,56,48,36,26,25,14,33,36,42,40,58,76,89];
+
