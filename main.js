4d23e10b14b962287e5a19b8cab401838a8a1de6
Ednial <ericxonb@gmail.com>
2022-05-05 08:32:24 -0500

    add forEach and filter functions

--- main.js
+++ main.js
@@ -12,3 +12,41 @@ const companies=[{name:"Company one",category:"Finance",start:1981,end:2003},
 
 const ages=[12,56,48,36,26,25,14,33,36,42,40,58,76,89];
 
+//Normal for loop
+for (let i = 0; i < companies.length; i++) {
+    console.log(companies[i]);    
+}
+//forEach -------------->Better way to trough an Array
+console.log("ForEach: ");
+companies.forEach(function (Company) { //We can also get (index,companie,name...wherever)
+    console.log(Company.name);
+});
+
+//Filter with for loop
+let canDrink=[];
+for (let i = 0; i < ages.length; i++) {
+    if(ages[i]>=21){
+        canDrink.push(ages[i]);
+    }
+}
+console.log("Filter with For");
+console.log(canDrink);
+
+//Filter like forEach
+const canDrink1=ages.filter(function (age) {
+    if(age>=21){
+        return true;
+    }
+});
+console.log("Filter with ForEAch ");
+console.log(canDrink1);
+
+//Filter with arrow function BEST WAY
+const canDrink2=ages.filter(age=> age>=21);  //ARROW FUNCTION => CONDITION age>=21
+console.log("Filter with arrowFunction");
+console.log(canDrink2);
+
+//Filter Reatail(category) companies
+console.log("Filter Reatail(category) companies:")
+const retailCompanies=companies.filter(company=>company.category =="Retail");
+console.log(retailCompanies);
